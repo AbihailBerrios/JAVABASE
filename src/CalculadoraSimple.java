a4e88185b407c34d5e8719d41b3b9ec0febb56b4
Eliezer Josué Jarquin Mairena <eliezer.jarquin405@std.uni.edu.ni>
2025-06-24 15:36:21 -0600

    se agrego multiplicacion

--- src/CalculadoraSimple.java
+++ src/CalculadoraSimple.java
@@ -10,16 +10,43 @@ public class CalculadoraSimple {
         System.out.println("Ingrese el segundo número:");
         int b = scanner.nextInt();
 
-        // Suma
-        int suma = a + b;
-        System.out.println("Suma: " + suma);
+        import java.util.Scanner;
 
-        // Resta
-        int resta = a - b;
-        System.out.println("Resta: " + resta);
+public class CalculadoraSimple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = scanner.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = scanner.nextInt();
+
+        System.out.println("Seleccione la operación:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        
+        System.out.print("Opción: ");
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                int suma = a + b;
+                System.out.println("Suma: " + suma);
+                break;
+
+            case 2:
+                int resta = a - b;
+                System.out.println("Resta: " + resta);
+                break;
+
+           
+
+            default:
+                System.out.println("Opción no válida.");
+        }
 
         scanner.close();
-
-        
     }
 }
+
