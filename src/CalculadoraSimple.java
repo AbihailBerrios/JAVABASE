a1f918ac4f58be507698b87a5e5e28fdc21d6e6d
Abihailb <genesis.perez020@std.uni.edu.ni>
2025-06-24 15:04:45 -0600

    se complemento el menu

--- src/CalculadoraSimple.java
+++ src/CalculadoraSimple.java
@@ -14,9 +14,11 @@ public class CalculadoraSimple {
         System.out.println("1. Suma");
         System.out.println("2. Resta");
         System.out.println("3. División");
+        System.out.println("4. Multiplicación");
+        int opcion = scanner.nextInt();
         
         switch (opcion) {
-            case 1://operacion de suma
+            case 1://operacion suma
                 int suma = a + b;
                 System.out.println("Suma: " + suma);
                 break;
